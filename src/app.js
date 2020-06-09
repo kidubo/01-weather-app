775d1916a14b0d72ae7849164771679a40b8b27f
kidubo <innocentleonard0007@gmail.com>
2020-06-09 11:37:57 +0300

    sixth commit

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ app.get('/weather', (req, res)=>{
         })
     }
 
-    geocode(req.queryaddress, (error, {latitude, longitude, location}={}) => {
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
         if(error){
             return res.send({ error })
         }
